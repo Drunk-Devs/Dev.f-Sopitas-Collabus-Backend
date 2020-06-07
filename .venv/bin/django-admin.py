5cc89e5dca5fb8c56699fca062c69b4bd184ecc5
jadeist <nauseas@protonmail.com>
2020-06-06 20:11:15 -0500

    django project

--- .venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/jade/Desktop/collab-us/github/Dev.f_Sopitas_Backend/.venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
